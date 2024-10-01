5fc23cdc2e8118861584c897888d3c14fe2892dc
ZHANG-png-png <zge29406@gmail.com>
2024-10-01 23:48:00 +0200

    Add .gitignore and module2 directory

---
+++ ZHANG-png-png/module2/mysupertools/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mysupertools',
+    version='0.1',
+    packages=find_packages(),
+    description='A package to multiply two numbers',
+    author='ZHANG-png-png',
+    author_email='zge29406@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
