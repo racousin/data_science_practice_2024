481333fe852077a68a0a5d00b39defc1886713d5
manjotgeek17 <singh.manjot1793@gmail.com>
2024-10-01 22:30:04 +0200

    Mon commit pour l'exo 1 du module 2

---
+++ manjotgeek17/module2/mysupertools/build/lib/tool/operation_a_b.py
@@ -0,0 +1,7 @@
+def multiply(a, b):
+    try:
+        result = a * b
+        return result
+    except Exception:
+        return "error"
+
---
+++ manjotgeek17/module2/mysupertools/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+
+setup(
+    name='mysupertools',
+    version='0.1',
+    description='Package pour multiplier 2 nombre ou renvoyer erreur',
+    author='Manjot Singh',
+    author_email='singh.manjot1793@gmail.com',
+    packages=find_packages(),
+)
