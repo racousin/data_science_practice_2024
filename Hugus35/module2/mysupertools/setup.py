bc0df1c58e059b509a733e57efdb99152b35fc98
Hugus35 <hugobonfils35@gmail.com>
2024-10-02 15:26:58 +0200

    Correction

---
+++ Hugus35/module2/mysupertools/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mysupertools',
+    version='0.1',
+    description='The submission for module 2',
+    author='Hugus35',
+    author_email='hugo.bonfils.1@etu.sorbonne-universite.fr',
+    packages=find_packages()
+)
