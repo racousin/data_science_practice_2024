58f9255cac41c8d8d3385a2f38e49ba6bfc5c9dd
Elie_Lelouche <elielelouche@gmail.com>
2024-10-07 21:23:27 +0200

    trying to install locally the mysupertools package but not succeding

---
+++ stabilo140/module2/mysupertools/mysupertools/tools/operation_a_b.py
@@ -0,0 +1,6 @@
+def multiply(a, b):
+    if isinstance(a,[float, int]) and isinstance(b, [float, int]):
+        return a*b
+    else:
+        return "error"
+    
---
+++ stabilo140/module2/mysupertools/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages*
+
+setup(
+    name="mysupertools",
+    version="0.1",
+    description="A simple Python package",
+    author="Elie Lelouche",
+    author_email="elielelouche@gmail.com",
+    packages=["mysupertols"]
+)
