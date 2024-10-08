6387e0a5d350f717863569f528346f1c898c4b6b
Kassim <berthekassime@gmail.com>
2024-10-08 17:30:27 +0200

    changer print en return

--- mysupertools/mysupertools/tool/.ipynb_checkpoints/operation_a_b-checkpoint.py
+++ mysupertools/mysupertools/tool/.ipynb_checkpoints/operation_a_b-checkpoint.py
@@ -2,9 +2,7 @@
 def multiply(a,b):
     M=[int, float]
     if (type(a) in M) and (type(b) in M):
-    #if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        #raise ValueError("Erreur dans les arguments.")
         return a*b
     else:
-        print("error")
+        return "error"
         
