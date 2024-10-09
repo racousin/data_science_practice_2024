ca191778946aa0e9e5f08975d8135eea82f5c6dc
Elie_Lelouche <elielelouche@gmail.com>
2024-10-09 11:24:37 +0200

    bug on the tests, saying the isinstance second argument is not a type, tuple of types or union argument

--- stabilo140/module2/mysupertools/setup.py
+++ stabilo140/module2/mysupertools/setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages*
+from setuptools import setup, find_packages
 
 setup(
     name="mysupertools",
-    version="0.1",
+    version="1.0",
     description="A simple Python package",
     author="Elie Lelouche",
     author_email="elielelouche@gmail.com",
-    packages=["mysupertols"]
+    packages=find_packages()
 )
