69853b353e0c6a238665271d146824809333e760
TarekDJAKER <djakertarek@gmail.com>
2024-11-18 20:48:04 +0100

    Update module2

---
+++ TarekDjaker/Module2/mysupertools/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='mysupertools',
+    version='1.0',
+    description='A package that multiplies two values or returns an error if not possible',
+    author='Tarek Djaker',
+    author_email='youremail@example.com',
+    url='https://github.com/yourusername/mypackage',
+    packages=['mypackage'],
+)
