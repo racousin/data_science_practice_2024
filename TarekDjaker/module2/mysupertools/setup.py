d4bd61a8fd031c6abab5b4c3b3824c1929ede961
TarekDJAKER <djakertarek@gmail.com>
2024-11-18 20:34:16 +0100

    Update module2

--- TarekDjaker/module2/mysupertools/setup.py
+++ TarekDjaker/module2/mysupertools/setup.py
@@ -1,9 +1,13 @@
-
+# setup.py
 from setuptools import setup, find_packages
 
 setup(
-    name="mysupertools",
-    version="0.1",
+    name='mysupertools',
+    version='0.1',
     packages=find_packages(),
-    install_requires=[],  # Add dependencies here if needed
+    install_requires=[],  # Liste des dépendances si nécessaire
+    author='Your Name',
+    description='A simple package for multiplying two values',
+    license='MIT',
+    url='https://github.com/username/mysupertools',  # Mettez l'URL correcte de votre dépôt GitHub
 )
